1d4f0e408a3fbbe055c955c613db7a99f0aa474e
Alexander <DrA1ex@users.noreply.github.com>
2024-09-01 20:35:07 +0500

    WebUI: Fix PWA installation with query params

--- www/src/index.js
+++ www/src/index.js
@@ -1,6 +1,38 @@
 import {DEFAULT_ADDRESS} from "./constants.js";
 import {Application} from "./application.js";
 
+function urlCombine(base, path, ...parts) {
+    const result = new URL(path, base).href;
+    if (!parts.length) return result;
+
+    return urlCombine(result, parts.shift(), ...parts);
+}
+
+function updateManifest(json) {
+    for (const [key, value] of Object.entries(json)) {
+        if (key === "src") json[key] = urlCombine(window.location.origin, window.location.pathname, value);
+        else if (value instanceof Array) value.forEach(v => updateManifest(v));
+        else if (value instanceof Object) updateManifest(value);
+    }
+}
+
+try {
+    const response = await fetch("./site.webmanifest");
+    if (!response.ok) console.error("Unable to load manifest:", response.statusText);
+
+    const json = await response.json();
+    updateManifest(json);
+    json.start_url = window.location.href;
+
+    const link = document.createElement("link");
+    link.rel = "manifest";
+    link.href = "data:application/manifest+json;charset=utf-8," + encodeURIComponent(JSON.stringify(json, null, 2));
+
+    document.head.appendChild(link);
+} catch (e) {
+    console.error("Unable to load manifest", e);
+}
+
 if ("serviceWorker" in navigator) {
     try {
         const registration = await navigator.serviceWorker.register("./service_worker.js");

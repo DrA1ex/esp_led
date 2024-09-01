11f7fa96b3424bde4a69d5cf9ff399dac9432f40
Alexander <DrA1ex@users.noreply.github.com>
2024-09-01 20:55:49 +0500

    WebUI: Refactor PWA installation with query params

--- www/src/index.js
+++ www/src/index.js
@@ -1,37 +1,16 @@
 import {DEFAULT_ADDRESS} from "./constants.js";
 import {Application} from "./application.js";
+import * as ManifestUtils from "./utils/manifest.js";
 
-function urlCombine(base, path, ...parts) {
-    const result = new URL(path, base).href;
-    if (!parts.length) return result;
+await ManifestUtils.makeManifest("./site.webmanifest");
 
-    return urlCombine(result, parts.shift(), ...parts);
-}
+const params = new Proxy(new URLSearchParams(window.location.search), {
+    get: (searchParams, prop) => searchParams.get(prop),
+});
 
-function updateManifest(json) {
-    for (const [key, value] of Object.entries(json)) {
-        if (key === "src") json[key] = urlCombine(window.location.origin, window.location.pathname, value);
-        else if (value instanceof Array) value.forEach(v => updateManifest(v));
-        else if (value instanceof Object) updateManifest(value);
-    }
-}
-
-try {
-    const response = await fetch("./site.webmanifest");
-    if (!response.ok) console.error("Unable to load manifest:", response.statusText);
-
-    const json = await response.json();
-    updateManifest(json);
-    json.start_url = window.location.href;
-
-    const link = document.createElement("link");
-    link.rel = "manifest";
-    link.href = "data:application/manifest+json;charset=utf-8," + encodeURIComponent(JSON.stringify(json, null, 2));
-
-    document.head.appendChild(link);
-} catch (e) {
-    console.error("Unable to load manifest", e);
-}
+const protocol = window.location.protocol === "https:" ? "wss" : "ws";
+const host = params.host ?? window.location.hostname;
+const gateway = `${protocol}://${host !== "localhost" ? host : DEFAULT_ADDRESS}/ws`;
 
 if ("serviceWorker" in navigator) {
     try {
@@ -41,14 +20,6 @@ if ("serviceWorker" in navigator) {
         console.log("ServiceWorker registration failed: ", e);
     }
 }
-
-const params = new Proxy(new URLSearchParams(window.location.search), {
-    get: (searchParams, prop) => searchParams.get(prop),
-});
-
-const protocol = window.location.protocol === "https:" ? "wss" : "ws";
-const host = params.host ?? window.location.hostname;
-const gateway = `${protocol}://${host !== "localhost" ? host : DEFAULT_ADDRESS}/ws`;
 
 const StatusElement = document.getElementById("status");
 
